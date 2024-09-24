8c441b6a048b29cefc54fbbdc2ae785915f5fbb5
leoramosa <leo.ramos.ari@gmail.com>
2024-09-24 14:05:24 -0300

    new form login

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Banner from "@/components/Banner/Banner";
+import CardExerciseBody from "@/components/CardExerciseBody/CardExerciseBody";
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      <CardExerciseBody />
+    </>
+  );
+}
