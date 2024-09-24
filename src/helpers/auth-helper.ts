a3110ed9fb5d5ee063dfe32e36493f279b4912d3
leoramosa <leo.ramos.ari@gmail.com>
2024-09-24 19:38:43 -0300

    change ok, m, m

--- src/helpers/auth-helper.ts
+++ src/helpers/auth-helper.ts
@@ -3,13 +3,16 @@ import { toast } from "sonner";
 
 export async function login(userData: ILoginProps): Promise<ILoginResponse> {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/signin`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      }
+    );
 
     if (!response.ok) {
       const errorResponse = await response.json();
