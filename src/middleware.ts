b4c99375c004fa59de58535615904786c9abd002
leoramosa <leo.ramos.ari@gmail.com>
2024-10-18 05:21:45 -0300

    final

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,6 +45,7 @@ export function middleware(req: NextRequest) {
   // Validar acceso a rutas restringidas según el rol
   if (decodedToken) {
     const adminRoutes = [
+      "/dashboard",
       "/dashboard/exercise",
       "/dashboard/reports",
       "/dashboard/routine",
