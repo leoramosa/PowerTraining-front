5c4d66ec5aef6ee7ee2508fbd307152af7e8bd50
leoramosa <leo.ramos.ari@gmail.com>
2024-10-18 15:52:00 -0300

    final

--- src/components/Pages/DashboardPage/DashboardPage.tsx
+++ src/components/Pages/DashboardPage/DashboardPage.tsx
@@ -1,19 +1,11 @@
 "use client";
-import { useAuthStore } from "@/stores/userAuthStore";
 import DashboardAdminPage from "../DashboardAdminPage/DashboardAdminPage";
-import DashboardClientPage from "../DashboardClientPage/DashboardClientPage";
 
 const DashboardPage = () => {
-  const { user } = useAuthStore(); // Obtenemos el token aquí
-
   return (
     <div className="relative">
       <div className="">
-        {user?.role === "Admin" ? (
-          <DashboardAdminPage />
-        ) : (
-          <DashboardClientPage />
-        )}
+        <DashboardAdminPage />
       </div>
     </div>
   );
